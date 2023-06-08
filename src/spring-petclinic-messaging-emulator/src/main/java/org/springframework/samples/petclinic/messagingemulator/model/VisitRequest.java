2b13cf055931fb8f75ad9faae217cec767e482cc
Gitte Vermeiren <gitte.vermeiren@gmail.com>
2023-06-08 11:14:08 +0200

    Update VisitRequest.java messaging emulator

--- src/spring-petclinic-messaging-emulator/src/main/java/org/springframework/samples/petclinic/messagingemulator/model/VisitRequest.java
+++ src/spring-petclinic-messaging-emulator/src/main/java/org/springframework/samples/petclinic/messagingemulator/model/VisitRequest.java
@@ -1,12 +1,12 @@
 package org.springframework.samples.petclinic.messagingemulator.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.validation.constraints.Size;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
